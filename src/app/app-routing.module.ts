3b38592d1ac7874c47be399f7b2256c9baecc248
kaaneneskpc <kaaneneskpc1@gmail.com>
2021-03-16 12:32:46 +0300

    rentAcarAngular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarComponent } from './components/car/car.component';
 import { RentalComponent } from './components/rental/rental.component';
 
 const routes: Routes = [
-  {path:"",pathMatch:"full",component:RentalComponent},
-  {path:"rentals",component:RentalComponent}
+  {path:"",pathMatch:"full",component:CarComponent},
+  {path:"cars",component:CarComponent},
+  {path:"cars/rental/:carId",component:CarComponent}
 ];
 
 @NgModule({
